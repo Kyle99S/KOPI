6eb78866b43d4d676d23d15de2063437b161f005
Kyle99S <kyleserafico@gmail.com>
2025-01-14 14:29:44 +0800

    intial
    initial commit

---
+++ sections/footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>locations</h3>
+            <a href="#">Pasay</a>
+            <a href="#">Parañaque</a>
+            <a href="#">Las Piñas</a>
+            <a href="#">Manila</a>
+            <a href="#">Cavite</a>
+        </div>
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="#home">Home</a>
+            <a href="#offers">Offers</a>
+            <a href="#about">About</a>
+            <a href="#menu">Menu</a>
+            <a href="#extra">Toppings</a>
+            <a href="#review">Reviews</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#">+123-456-7890</a>
+            <a href="#">+111-222-3333</a>
+        </div>
+
+        <div class="box">
+            <h3>follow me</h3>
+            <a href="#">facebook</a>
+            <a href="#">twitter</a>
+            <a href="#">instagram</a>
+        </div>
+
+    </div>
+
+    <div class="credit"> copyright @ 2022 by <span>Kyle Serafico</span> </div>
+
+</section>
---
+++ sections/home.php
@@ -0,0 +1,52 @@
+<section class="home" id="home">
+
+    <div class="swiper-container home-slider">
+
+        <div class="swiper-wrapper wrapper">
+
+            <div class="swiper-slide slide">
+                <div class="content" >
+                    <span>our special Coffee</span>
+                    <h3>Caramel Latte</h3>
+                    <p>The best smoothie that will change your whole morning routine!</p>
+                    <a href="#menu" class="btn">order now</a>
+                </div>
+                <div class="image" >
+                    <img src="images/coffee/crml.png" alt="">
+                </div>
+            </div>
+
+         
+            <div class="swiper-slide slide">
+                <div class="content">
+                    <span>our big secret</span>
+                    <h3>Fresh Coffee beans</h3>
+                    <p>the secret behind our delicious coffee is that we use fresh Coffee Beans 100% organic with no chemicals!</p>
+                    <a href="#menu" class="btn">order now</a>
+                </div>
+                
+                <div class="image">
+                    <img src="images/coffee/coffeebean.png" alt="">
+                </div>
+            </div>
+
+            <div class="swiper-slide slide">
+                <div class="content">
+                    <span>Our goal is to make our customers happy</span>
+                    <h3>Extra Toppings</h3>
+                    <p>You can add whatever toppings you want!</p>
+                    <a href="#menu" class="btn">order now</a>
+                </div>
+                <div class="image">
+                    <img src="images/coffee/chocowhipped.png" alt="">
+                </div>
+            </div>
+        </div>
+        <!-- Three buttons -->
+        <div class="swiper-pagination"></div>
+
+    </div>
+  
+
+
+</section>
